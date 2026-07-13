98fc37a41b152599f1cda74435cb2821a0307071
csseduapps-crypto <css.edu.app@gmail.com>
2026-07-13 14:21:44 +0500

    fifth

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!\n This is a Flutter app.'),
+          child: Text('Hello World!\n This is a Flutter.'),
         ),
       ),
     );
